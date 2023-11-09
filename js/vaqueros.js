b2f1e22e12515d29a7560845fdd47f2d97306195
AndersonMunoz <anderson180@hotmail.es>
2023-11-08 21:29:55 -0500

    Rango con salto

--- js/vaqueros.js
+++ js/vaqueros.js
@@ -1,24 +1,52 @@
 let rango = document.getElementById("rango");
 let recorrido = document.querySelector(".recorrido");
-let position = 100;
+let imgFondo = document.querySelector(".img-fondo");
+let position = 0;
+let direction = 0;
+let jumping = false;
 
-window.addEventListener("keydown", function(event) {
-    const recorridoWidth = recorrido.clientWidth;
-    const rangoWidth = rango.clientWidth;
-
-    if (event.key === "ArrowRight") {
-        position += 50; 
+function move() {
+    if (direction === 1 && position < window.innerWidth - rango.offsetWidth) {
+        position += 5;
         rango.style.transform = "scaleX(1)";
-    } else if (event.key === "ArrowLeft") {
-        position -= 50; 
-        rango.style.transform = "scaleX(-1)"; 
-    } else if (event.key === "ArrowUp") {
-        position -= 50; 
-        rango.style.transform = "scaleY(1)"; 
-    } 
-
-    position = Math.max(0, Math.min(recorridoWidth - rangoWidth, position));
-
+    } else if (direction === -1 && position > 0) {
+        position -= 5;
+        rango.style.transform = "scaleX(-1)";
+    }
 
     rango.style.left = position + "px";
+    imgFondo.style.backgroundPositionX = -(position * 2) + "px"; 
+}
+
+function jump() {
+    if (!jumping) {
+        jumping = true;
+        rango.style.bottom = "100px";
+        setTimeout(function() {
+            rango.style.bottom = "0px";
+            jumping = false;
+        }, 500);
+    }
+}
+
+window.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowRight") {
+        direction = 1;
+    } else if (event.key === "ArrowLeft") {
+        direction = -1;
+    } else if (event.key === " ") {
+        jump();
+    }
 });
+
+window.addEventListener("keyup", function(event) {
+    if (event.key === "ArrowRight" && direction === 1) {
+        direction = 0;
+    } else if (event.key === "ArrowLeft" && direction === -1) {
+        direction = 0;
+    }
+});
+
+setInterval(move, 20);
+
+
