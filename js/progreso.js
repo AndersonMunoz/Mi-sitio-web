75a89a9c81e7b0e2c516fc4c12b3a946cc39843a
AndersonMunoz <anderson180@hotmail.es>
2023-11-28 18:53:10 -0500

    Barra de progreso mejorado final

--- js/progreso.js
+++ js/progreso.js
@@ -6,7 +6,6 @@ let votos_totales = document.getElementById('votos_totales')
 
 boton.addEventListener('click', function () {
     let total = 0;
-
     for (let i = 0; i < numero.length; i++) {
         if (numero[i].value === null || numero[i].value === "") {
             numero[i].value = 0;
@@ -14,20 +13,15 @@ boton.addEventListener('click', function () {
         total = total + parseFloat(numero[i].value);
     }
     votos_totales.innerHTML = total.toString();
-
     for (let k = 0; k < numero.length; k++) {
-        if (numero[k].value === null || numero[k].value === "" || parseFloat(numero[k].value) === 0) {
-            valor_por[k].innerHTML = 0;
-            barra[k].style.borderRadius = "5px";
-            barra[k].style.backgroundColor = "rgb(17, 197, 197)";
-            barra[k].style.width = porcentaje + "%";
-        } else {
-            let porcentaje = (parseFloat(numero[k].value) * 100) / total;
-            valor_por[k].innerHTML = Math.round(porcentaje).toString();
-            barra[k].style.borderRadius = "5px";
-            barra[k].style.backgroundColor = "rgb(17, 197, 197)";
-            barra[k].style.width = porcentaje + "%";
+        let porcentaje = 0;
+        if (total !== 0) {
+            porcentaje = (parseFloat(numero[k].value) * 100) / total;
         }
+        valor_por[k].innerHTML = Math.round(porcentaje).toString();
+        barra[k].style.borderRadius = "5px";
+        barra[k].style.backgroundColor = "rgb(17, 197, 197)";
+        barra[k].style.width = porcentaje + "%";
     }
 });
 
